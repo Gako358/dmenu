704fc9f7011dfdb2b62b0baaf5f0a188712d9fed
Gako358 <gako358@outlook.com>
2020-11-03 08:04:59 +0100

    light scheme

--- config.def.h
+++ config.def.h
@@ -3,20 +3,20 @@
 
 static int topbar                       = 1;            /* -b  option; if 0, dmenu appears at bottom     */
 static int centered                     = 1;            /* -c option; centers dmenu on screen */
-static int min_width                    = 500;          /* minimum width when centered */
+static int min_width                    = 700;          /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=14"
 };
 static const char *prompt               = NULL;         /* -p  option; prompt to the left of input field */
-static const char col_bg[]              = "#3c3836";    // Dark 
-static const char col_fg[]              = "#ebdbb2";    // White
-static const char col_sel[]             = "#458588";    // Blue
-static const char col_tag[]             = "#98971a";    // Green
+static const char col_bg[]              = "#ebdbb2";    // White 
+static const char col_fg[]              = "#282828";    // Dark
+static const char col_sel[]             = "#fb4934";    // Red
+static const char col_tag[]             = "#928374";    // Gray
 static const char *colors[SchemeLast][2] = {
 	/*                    fg          bg       */
 	[SchemeNorm]    = { col_fg,     col_bg  },
-	[SchemeSel]     = { col_fg,     col_sel },
+	[SchemeSel]     = { col_tag,    col_sel },
 	[SchemeOut]     = { col_fg,     col_bg  },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
